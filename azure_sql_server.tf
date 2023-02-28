8ac3f00057edd6f06e18f600c27479fa3e3d162b
Lucas Zanoni <91813099+Castrozan@users.noreply.github.com>
2023-02-28 20:13:38 -0300

    Add files via upload

---
+++ azure_sql_server.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_mssql_server" "sql" {
+  name                = "nome-do-servidor-do-banco"
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  version             = "12.0"
+
+  
+
+  administrator_login          = var.usuario_admin
+  administrator_login_password = var.password
+}
+
+resource "azurerm_mssql_database" "sql" {
+  name      = "nome-do-banco-de-dados"
+  server_id = azurerm_mssql_server.sql.id
+  collation = "SQL_Latin1_General_CP1_CI_AS"
+
+  auto_pause_delay_in_minutes = -1
+  max_size_gb                 = 32
+  min_capacity                = 10
+  read_replica_count          = 0
+  read_scale                  = false
+  sku_name                    = "GP_S_Gen5_10"
+  zone_redundant              = false
+}
